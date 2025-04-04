86eb68e62c588818aecdbf24ef09ff9ecf7c05aa
Liyan Zhao <return65535@qq.com>
2025-04-05 00:41:10 +0800

    1

---
+++ README.md
@@ -0,0 +1,13 @@
+# CraftMine Bug Fixes
+
+https://modrinth.com/mod/craftmine-fixes
+
+Critical Bugs:
+
+- Fixed "Soul Link" causes StackOverflowError crashing.
+
+Game Experience:
+
+- When you fell out of the world, you will respawn at the world bottom, instead of the original position.
+- [WIP] Pets wont attack other players in "Universal Anger".
+
--- src/main/java/com/github/zly2006/craftminefixes/PreLaunch.java
+++ src/main/java/com/github/zly2006/craftminefixes/PreLaunch.java
@@ -1,11 +1,14 @@
 package com.github.zly2006.craftminefixes;
 
+import net.fabricmc.loader.api.FabricLoader;
 import net.fabricmc.loader.api.entrypoint.PreLaunchEntrypoint;
 import net.minecraft.SharedConstants;
 
 public class PreLaunch implements PreLaunchEntrypoint {
     @Override
     public void onPreLaunch() {
-        SharedConstants.IS_RUNNING_IN_IDE = true;
+        if (FabricLoader.getInstance().isDevelopmentEnvironment()) {
+            SharedConstants.IS_RUNNING_IN_IDE = true;
+        }
     }
 }
