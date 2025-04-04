4685ba109812eeab6a0f624b378b183f1696f4df
Liyan Zhao <return65535@qq.com>
2025-04-04 17:06:47 +0800

    init

---
+++ src/main/java/com/github/zly2006/craftmine_fix_death/client/Craftmine_fix_deathClient.java
@@ -0,0 +1,10 @@
+package com.github.zly2006.craftmine_fix_death.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class Craftmine_fix_deathClient implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+}
