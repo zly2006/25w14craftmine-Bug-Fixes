56b15391fd51dbf9b7a556d6c8ea279ae31dddaf
Liyan Zhao <return65535@qq.com>
2025-04-06 09:42:58 +0800

    fix

--- src/main/java/com/github/zly2006/craftminefixes/mixin/MixinRegistryHolder.java
+++ src/main/java/com/github/zly2006/craftminefixes/mixin/MixinRegistryHolder.java
@@ -12,23 +12,31 @@ import net.minecraft.world.level.biome.Biome;
 import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
+
 import java.util.Optional;
 
 @Mixin(Holder.Reference.class)
 public class MixinRegistryHolder {
-    @Shadow private @Nullable ResourceKey<Object> key;
+    @Shadow
+    private @Nullable ResourceKey<Object> key;
+    @Unique
+    private Holder.Reference<Biome> cache;
 
-    @WrapMethod(
-            method = "is(Lnet/minecraft/tags/TagKey;)Z"
-    )
-    private boolean is(TagKey tagKey, Operation<Boolean> original) {
+    @SuppressWarnings("DataFlowIssue")
+    @WrapMethod(method = "is(Lnet/minecraft/tags/TagKey;)Z")
+    private boolean is(TagKey<Biome> tagKey, Operation<Boolean> original) {
         if (key.registry().getPath().contains("worldgen/biome") && key.location().getPath().contains("level")) {
+            if (cache != null) {
+                return cache.is(tagKey);
+            }
             if (CraftmineFixes.server != null && CraftmineFixes.server.isRunning()) {
                 var biome = CraftmineFixes.server.theGame().registryAccess().lookup(Registries.BIOME);
                 if (biome.isPresent()) {
                     Optional<Holder.Reference<Biome>> value = biome.get().get(ResourceLocation.withDefaultNamespace(key.location().getPath().split("/")[1]));
                     if (value.isPresent()) {
-                        return value.get().is(tagKey);
+                        cache = value.get();
+                        return cache.is(tagKey);
                     }
                 }
             }
